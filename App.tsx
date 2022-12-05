2bd7b28dd8f4ca80ddf1e798fa797371e76517b8
Nyx <nyxfor13days@gmail.com>
2022-12-06 00:44:33 +0530

    Setup Screens - Added Stack Navigator and Navigation Container in App.tsx - Created screens directory for screens

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+// Screen Imports
+import Home from "./screens/Home";
+
+// Stack Navigator
+const Stack = createNativeStackNavigator();
+
+export default () => {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+
+      <StatusBar style="light" />
+    </NavigationContainer>
   );
-}
+};
