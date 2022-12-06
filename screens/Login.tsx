1cf28dae22e687fb87b2489afc8a4205bf67e4a3
Nyx <nyxfor13days@gmail.com>
2022-12-06 15:55:54 +0530

    Modified Login.tsx

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -5,7 +5,7 @@ import { ScreenWrapper } from "../components";
 export default () => {
   return (
     <ScreenWrapper>
-      <VStack mt={10} space={6}>
+      <VStack py={10} space={6}>
         <Heading
           size="4xl"
           fontWeight="900"
@@ -15,17 +15,11 @@ export default () => {
           Login to your Eternal Bible
         </Heading>
 
-        <VStack space={1}>
-          <Text fontSize="xl">
-            Eternal Bible Project aims to provide ad-free, offline and authentic
-            bible reading. We are currently in beta and are working on adding
-            more feature.
-          </Text>
-          <Text fontSize="xl">
-            Please login to your account to access your saved notes and to sync
-            your progress across devices.
-          </Text>
-        </VStack>
+        <Text fontSize="xl">
+          Eternal Bible Project aims to provide ad-free, offline and authentic
+          bible reading. We are currently in beta and are working on adding more
+          feature.
+        </Text>
 
         <Button leftIcon={<Icon as={AntDesign} name="google" />}>
           Login with Google
