ccc710d984ffc878eb21e28dd2abc1561db21cc6
Nyx <nyxfor13days@gmail.com>
2022-12-06 12:01:08 +0530

    Added fonts in theme

--- theme/index.ts
+++ theme/index.ts
@@ -1,6 +1,8 @@
 import { extendTheme } from "native-base";
 import fontConfig from "./fontConfig";
+import fonts from "./fonts";
 
 export default extendTheme({
   fontConfig: fontConfig,
+  fonts: fonts,
 });
