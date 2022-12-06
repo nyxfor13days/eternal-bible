76b2e36561615dc9000fc2011415f511ce655612
Nyx <nyxfor13days@gmail.com>
2022-12-06 11:13:12 +0530

    Added NativeBaseProvider

--- App.tsx
+++ App.tsx
@@ -6,18 +6,21 @@ import type { RootParams } from "./@types/navigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "./screens";
+import { NativeBaseProvider } from "native-base";
 
 // Stack Navigator
 const Stack = createNativeStackNavigator<RootParams>();
 
 export default () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
 
-      <StatusBar style="light" />
-    </NavigationContainer>
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 };
