7ebfa566d7911e82b37bc1d0e07281f5e003cc8e
Nyx <nyxfor13days@gmail.com>
2022-12-06 19:00:42 +0530

    Changed Primary Color

--- theme/index.ts
+++ theme/index.ts
@@ -1,10 +1,12 @@
 import { extendTheme } from "native-base";
+import colors from "./colors";
 import components from "./components";
 import config from "./config";
 import fontConfig from "./fontConfig";
 import fonts from "./fonts";
 
 export default extendTheme({
+  colors: colors,
   fontConfig: fontConfig,
   fonts: fonts,
   components: components,
