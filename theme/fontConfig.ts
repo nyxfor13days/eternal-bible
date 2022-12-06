aacea5c8715bb39035cf07a921ec073d479bb620
Nyx <nyxfor13days@gmail.com>
2022-12-06 11:59:18 +0530

    Added fontConfig

--- theme/fontConfig.ts
+++ theme/fontConfig.ts
@@ -1,1 +1,40 @@
-export default {};
+export default {
+  Lato: {
+    100: {
+      normal: "Lato-Thin",
+      italic: "Lato-ThinItalic",
+    },
+    200: {
+      normal: "Lato-Light",
+      italic: "Lato-LightItalic",
+    },
+    300: {
+      normal: "Lato-light",
+      italic: "Lato-LightItalic",
+    },
+    400: {
+      normal: "Lato-Regular",
+      italic: "Lato-Italic",
+    },
+    500: {
+      normal: "Lato-Regular",
+      italic: "Lato-RegularItalic",
+    },
+    600: {
+      normal: "Lato-Bold",
+      italic: "Lato-BoldItalic",
+    },
+    700: {
+      normal: "Lato-Bold",
+      italic: "Lato-BoldItalic",
+    },
+    800: {
+      normal: "Lato-Black",
+      italic: "Lato-BlackItalic",
+    },
+    900: {
+      normal: "Lato-Black",
+      italic: "Lato-BlackItalic",
+    },
+  },
+};
